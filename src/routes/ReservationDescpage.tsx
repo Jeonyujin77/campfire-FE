3c8d050a5ccd1aaeb7d31a2a4effae5ba2d3d9b0
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2023-01-03 20:33:08 +0900

    STYLE: 예약 안내 페이지 파일 생성 #9
    - 예약 안내 페이지 파일 생성

---
+++ src/routes/ReservationDescpage.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const ReservationDescpage = () => {
+  return <Wrap>ReservationDescpage</Wrap>;
+};
+
+const Wrap = styled.div`
+  /* 헤더 크기에 따라 수정 필요 */
+  margin: 0px auto;
+  /* 헤더 아래 출력되도록 */
+  margin-top: 100px;
+  width: 1200px;
+  max-height: 100%;
+  min-height: 100vh;
+  border: 1px solid red;
+`;
+
+export default ReservationDescpage;
