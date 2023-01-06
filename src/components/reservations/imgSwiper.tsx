bd16f82589c739242f8645aa31de8037a3a8445a
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2023-01-06 13:57:39 +0900

    FEAT: img swiper 기능, CSS 변경 #17
    - img swiper 기능, CSS 변경

--- src/components/reservations/imgSwiper.tsx
+++ src/components/reservations/imgSwiper.tsx
@@ -9,63 +9,12 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 export interface DetailCampDesc {
-  campId: number;
-  img: string;
-  name: string;
-  address: string;
+  campMainImage: string;
+  campSubImages?: any;
 }
 
-const ImgSwiper = () => {
-  const dummyDatas = [
-    {
-      campId: 1,
-      img: 'https://ldb-phinf.pstatic.net/20210316_181/1615889940906JODyL_JPEG/03119896_%282%29.jpg?type=f804_408_60_sharpen',
-      name: '진주 캠핑소사이어티 럭셔리 글램핑&피크닉',
-      address: '경상남도 진주시',
-    },
-    {
-      campId: 2,
-      img: 'https://ldb-phinf.pstatic.net/20210316_181/1615889940906JODyL_JPEG/03119896_%282%29.jpg?type=f804_408_60_sharpen',
-      name: '진주 캠핑소사이어티 럭셔리 글램핑&피크닉',
-      address: '경상남도 진주시',
-    },
-    {
-      campId: 3,
-      img: 'https://ldb-phinf.pstatic.net/20210316_181/1615889940906JODyL_JPEG/03119896_%282%29.jpg?type=f804_408_60_sharpen',
-      name: '진주 캠핑소사이어티 럭셔리 글램핑&피크닉',
-      address: '경상남도 진주시',
-    },
-    {
-      campId: 4,
-      img: 'https://ldb-phinf.pstatic.net/20210316_181/1615889940906JODyL_JPEG/03119896_%282%29.jpg?type=f804_408_60_sharpen',
-      name: '진주 캠핑소사이어티 럭셔리 글램핑&피크닉',
-      address: '경상남도 진주시',
-    },
-    {
-      campId: 5,
-      img: 'https://ldb-phinf.pstatic.net/20210316_181/1615889940906JODyL_JPEG/03119896_%282%29.jpg?type=f804_408_60_sharpen',
-      name: '진주 캠핑소사이어티 럭셔리 글램핑&피크닉',
-      address: '경상남도 진주시',
-    },
-    {
-      campId: 6,
-      img: 'https://ldb-phinf.pstatic.net/20210316_181/1615889940906JODyL_JPEG/03119896_%282%29.jpg?type=f804_408_60_sharpen',
-      name: '진주 캠핑소사이어티 럭셔리 글램핑&피크닉',
-      address: '경상남도 진주시',
-    },
-    {
-      campId: 7,
-      img: 'https://ldb-phinf.pstatic.net/20210316_181/1615889940906JODyL_JPEG/03119896_%282%29.jpg?type=f804_408_60_sharpen',
-      name: '진주 캠핑소사이어티 럭셔리 글램핑&피크닉',
-      address: '경상남도 진주시',
-    },
-    {
-      campId: 8,
-      img: 'https://ldb-phinf.pstatic.net/20210316_181/1615889940906JODyL_JPEG/03119896_%282%29.jpg?type=f804_408_60_sharpen',
-      name: '진주 캠핑소사이어티 럭셔리 글램핑&피크닉',
-      address: '경상남도 진주시',
-    },
-  ];
+const ImgSwiper = ({ campMainImage, campSubImages }: DetailCampDesc) => {
+  const images = [campMainImage, ...campSubImages];
 
   return (
     <>
@@ -78,9 +27,9 @@ const ImgSwiper = () => {
         loop={true}
         pagination={{ clickable: true }}
       >
-        {dummyDatas.map(data => (
-          <StyledSwiperSlide key={data.campId}>
-            <ImgInSwiper data={data} />
+        {images.map(image => (
+          <StyledSwiperSlide key={image}>
+            <CampImg src={image} />
           </StyledSwiperSlide>
         ))}
       </StyledSwiper>
@@ -90,7 +39,7 @@ const ImgSwiper = () => {
 
 const StyledSwiper = styled(Swiper)`
   width: 1200px;
-  height: 400px;
+  height: 600px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -104,6 +53,12 @@ const StyledSwiperSlide = styled(SwiperSlide)`
   justify-content: center;
   flex-direction: row;
   text-align: center;
+`;
+
+const CampImg = styled.img`
+  object-fit: fill;
+  width: 1200px;
+  height: 600px;
 `;
 
 export default ImgSwiper;
