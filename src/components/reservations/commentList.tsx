8cc110278cf61b4f17d7586fc4efe9a34b3586d8
Jeonyujin77 <wjsdbwls7777@gmail.com>
2023-01-19 18:14:54 +0900

    [FIX] 캠핑장상세 리뷰 더보기-데이터중복출력버그 수정

--- src/components/reservations/commentList.tsx
+++ src/components/reservations/commentList.tsx
@@ -18,11 +18,16 @@ interface CommentProps {
 
 const CommentList = (props: CommentProps) => {
   const dispatch = useAppDispatch();
+  const [isClicked, setIsClicked] = useState(false);
   const [prevPage, setPrevPage] = useState(1);
   const [cmtPage, setCmtPage] = useState(1);
   const [moreCnt, setMoreCnt] = useState(true);
   const [comments, setComments] = useState<CommentInfo[]>([]);
   const { campId } = props;
+
+  useEffect(() => {
+    getCampReviews(cmtPage);
+  }, [cmtPage]);
 
   const getCampReviews = (page: number) => {
     dispatch(__getCampReviews({ campId, pageno: page })).then(res => {
@@ -31,14 +36,19 @@ const CommentList = (props: CommentProps) => {
       // 조회 성공
       if (type === 'getCampReviews/fulfilled') {
         const reviews = payload.reviews;
-        // 조회결과가 있고 이전페이지와 현페이지가 다를때
-        if (reviews.length !== 0 && prevPage !== cmtPage) {
-          setComments([...comments, ...payload.reviews]);
-        }
-        // 조회결과가 있고 이전페이지와 현페이지가 같을때-중복으로 리뷰가 생기는 문제 수정
-        else if (reviews.length !== 0 && prevPage === cmtPage) {
+
+        // 첫 조회
+        if (reviews.length !== 0 && cmtPage === 1) {
           setComments([...payload.reviews]);
-        } else {
+        }
+        // 조회결과가 있고 이전페이지와 현페이지가 다를때 그리고 사용자가 더보기를 클릭한 경우에만
+        // 페이지 리렌더링 시 데이터가 중복되는 버그가 발생하여 isClicked로 클릭유무를 판단하여 클릭한 경우에만 아래 데이터를 출력함
+        else if (reviews.length !== 0 && prevPage !== cmtPage && isClicked) {
+          setComments([...comments, ...payload.reviews]);
+          setIsClicked(false);
+        }
+        // 조회 결과가 없는 경우 더보기버튼 숨김
+        else {
           setMoreCnt(false);
         }
       }
@@ -49,11 +59,9 @@ const CommentList = (props: CommentProps) => {
     });
   };
 
-  useEffect(() => {
-    getCampReviews(cmtPage);
-  }, [cmtPage]);
-
+  // 리뷰 더보기 클릭 시
   const getCommentsByPageno = (prev: number, next: number) => {
+    setIsClicked(true);
     setPrevPage(prev);
     setCmtPage(next);
     getCampReviews(next);
@@ -71,7 +79,7 @@ const CommentList = (props: CommentProps) => {
           />
         ))
       ) : (
-        <>작성된 리뷰가 없습니다.</>
+        <>작성된 리뷰가 없습니다. 리뷰를 등록해보세요.</>
       )}
       {moreCnt ? (
         <MoreComment onClick={() => getCommentsByPageno(cmtPage, cmtPage + 1)}>
