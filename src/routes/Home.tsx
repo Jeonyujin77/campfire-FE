e9134e7a138ba73ebd37b5ce4cca25e95bf7a089
escape-engineering <117638805+escape-engineering@users.noreply.github.com>
2023-02-03 02:35:38 +0900

    [FEAT] 메인페이지 배너 구현 #175

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,6 +5,9 @@ import { useEffect } from 'react';
 //컴포넌트
 import CheckAuth from '../components/common/CheckAuth';
 import CampItemList from '../components/camps/campItemList';
+//이미지
+import upArrowOrange from '../asset/upArrowOrange.png';
+import BannerSwiper from '../components/banner/bannerSwiper';
 
 const Home = () => {
   //페이지 이동 시 스크롤바 상단으로 이동
@@ -16,7 +19,30 @@ const Home = () => {
   return (
     <Wrap>
       <CheckAuth />
-      <HomeTitle>쉽고 편한 캠핑장 예약 시스템 캠프파이어</HomeTitle>
+      <HomeTitle>
+        <div>
+          <img
+            style={{
+              width: '25px',
+              height: '25px',
+              transform: 'rotate(-90deg)',
+            }}
+            src={upArrowOrange}
+            alt="화살표"
+          />
+          쉽고 편한 캠핑장 예약 시스템 캠프파이어
+          <img
+            style={{
+              width: '25px',
+              height: '25px',
+              transform: 'rotate(90deg)',
+            }}
+            src={upArrowOrange}
+            alt="화살표"
+          />
+        </div>
+      </HomeTitle>
+      <BannerSwiper />
       <CampItemList />
     </Wrap>
   );
@@ -24,22 +50,34 @@ const Home = () => {
 
 const Wrap = styled.div`
   margin: 0px auto;
-  margin-top: 120px;
+  margin-top: 60px;
   width: 1200px;
   max-height: 100%;
   min-height: 100vh;
   @media (max-width: 1200px) {
     width: 100%;
+    margin-top: 0px;
   }
 `;
 
 const HomeTitle = styled.div`
+  height: 90px;
   display: flex;
-  align-items: center;
   justify-content: center;
   font-size: 25px;
   font-weight: bold;
   color: rgb(254, 128, 44);
+  div {
+    width: 600px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    border-bottom-left-radius: 45px;
+    border-bottom-right-radius: 45px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
 
   @media (max-width: 600px) {
     font-size: 18px;
